87fb4cc0191033d7570265af770f9d5a24700574
Abdur-Rahman-Argon <106532958+Abdur-Rahman-Argon@users.noreply.github.com>
2022-07-03 07:39:35 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
-const express = require('express')
-const  cors = require('cors')
-const app = express()
+const express = require("express");
+const cors = require("cors");
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors());
 
-app.use(cors())
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-app.get('/products/:id', (req, res, next)=>{
-  res.send({'This is server is running'})
-})
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  console.log("mongodb connected");
+  client.close();
+});
 
-app.listen(port,()=>{
-  console.log('server running port', port)
-})
+app.get("/products/:id", (req, res, next) => {
+  res.send("This is server is running");
+});
+
+app.listen(port, () => {
+  console.log("server running port", port);
+});
