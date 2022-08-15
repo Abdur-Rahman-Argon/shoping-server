5627745b78feb498f582d452d49c983dc96d46cd
Abdur-Rahman-Argon <106532958+Abdur-Rahman-Argon@users.noreply.github.com>
2022-08-15 18:03:58 +0600

    user delete api

--- index.js
+++ index.js
@@ -133,6 +133,14 @@ async function run() {
       res.send({ result });
     });
 
+    // user delete api
+    app.delete("/user/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const result = await userCollection.deleteOne(filter);
+      res.send({ result });
+    });
+
     //-------------------------------------------------
 
     //
